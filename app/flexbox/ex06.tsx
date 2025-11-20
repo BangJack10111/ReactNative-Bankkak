970ec85b99931827266fbff7557a75a756d20211
BangJack10111 <Amorntape2548l@gmail.com>
2025-11-20 16:34:50 +0700

    ไม่บอก

---
+++ app/flexbox/ex06.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Ex06() {
+  return (
+    <View style={{
+      flex: 1, 
+      flexDirection: 'column',
+      justifyContent: 'center',   
+      alignItems: 'center'        
+    }}>
+      <View style={{
+        width: 100, height: 100, 
+        backgroundColor: 'powderblue'}} 
+      />
+
+      <View style={{
+        width: 100, height: 100, 
+        backgroundColor: 'skyblue'}} 
+      />
+
+      <View style={{
+        width: 100, height: 100, 
+        backgroundColor: '#9013FE'}} 
+      />
+    </View>
+  );
+}
